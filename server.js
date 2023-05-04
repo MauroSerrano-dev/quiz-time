c25c014c80d61277cf48f08749f1e8d4338d9c62
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-04 21:26:04 +0100

    redirect http to https

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
     res.send('Olá, mundo!');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Servidor iniciado na porta ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 80, () => {
+    console.log(`Servidor iniciado na porta ${process.env.PORT || 80}`);
 });
