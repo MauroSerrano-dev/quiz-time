1869652d035e26a25cb0a90afa74234e063c01bf
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-04 22:35:52 +0100

    redirect http to https

---
+++ server.js
@@ -0,0 +1,9 @@
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  } else {
+    next();
+  }
+});
