591f5cd91c5fe2ed11b2c397207275998143a173
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-06 13:11:50 +0100

    'teste'

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require('express');
-const sslRedirect = require('express-sslify');
+const next = require('next');
 
-const server = express();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(sslRedirect());
-}
+const handle = app.getRequestHandler();
 
-// Restante do código do seu servidor
+app
+  .prepare()
+  .then(() => {
 
-module.exports = server;
+    const server = express();
+
+    server.use((req, res, next) => {
+      const hostname = req.hostname === 'www.app.domain.com' ? 'app.domain.com' : req.hostname;
+
+      if (req.headers['x-forwarded-proto'] === 'http' || req.hostname === 'www.app.domain.com') {
+        res.redirect(301, `https://${hostname}${req.url}`);
+        return;
+      }
+
+      res.setHeader('strict-transport-security', 'max-age=31536000; includeSubDomains; preload');
+      next();
+    });
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(
+      4242,
+      error => {
+        if (error) throw error;
+        console.error('Listening on port 4242');
+      }
+    );
+
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
