4585d6e24170bbbcdc7f061265c65ecf3b1241dc
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-06 14:06:31 +0100

    'teste'

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
-/* const isDev = process.env.NODE_ENV !== 'production' */
+const isDev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   reactStrictMode: true,
-  /* async rewrites() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://quiz-maker.herokuapp.com/$1',
-      },
-    ];
-  }, */
-
   async redirects() {
     return [
       {
-        source: '/:path*',
-        permanent: true,
-        destination: 'https://quiz-maker.herokuapp.com/:path*',
+        source: '/*',
+        destination: `https://quiz-maker.herokuapp.com/:splat`,
+        permanent: !isDev,
       },
     ];
   },
