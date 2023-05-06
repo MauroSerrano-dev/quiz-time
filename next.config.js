9fa43cfb1f36345ab7d87cfa61efcea277c8b289
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-06 13:41:01 +0100

    teste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const sslRedirect = require('heroku-ssl-redirect').default;
+const isDev = process.env.NODE_ENV !== 'production'
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  
+  async rewrites() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://quiztime.pt/$1',
+      },
+    ];
+  },
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        permanent: true,
+        destination: 'https://quiztime.pt/:path*',
+      },
+    ];
+  },
+
+  webpack: (config, { isServer }) => {
+    // for production, force SSL/TLS
+    if (!isDev) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser';
+      config.plugins.push(new sslRedirect());
+    }
+
+    return config;
+  },
+};
