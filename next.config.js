ad6df1a8b7b9e543ef39745c2600134a082d0c41
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-06 14:40:00 +0100

    'teste'

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
-const sslRedirect = require('heroku-ssl-redirect').default;
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = sslRedirect();
+const withSslRedirect = require('heroku-ssl-redirect')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(
+  withSslRedirect({
+    async redirects() {
+      return [
+        {
+          source: '/about',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    },
+    webpack(config, { isServer }) {
+      if (!isServer) {
+        config.resolve.fallback.fs = false
+      }
+      return config
+    },
+    env: {
+      customKey: 'my-value',
+    },
+    future: {
+      webpack5: true,
+    },
+    poweredByHeader: false,
+  })
+)
