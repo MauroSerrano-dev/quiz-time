2608fdb2a3f7529406c4c8eecb4a477ee17adbeb
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-06 16:14:09 +0100

    aaaaaaaaaaaaa

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.prepare().then(() => {
         return handle(req, res);
     });
 
-    server.listen(3000, (err) => {
+    server.listen(process.env.PORT, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
+        console.log(`> Ready on http://localhost:${process.env.PORT}`);
     });
 });
