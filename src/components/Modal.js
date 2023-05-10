4d5f9c4ae6b7c610090956d9b81ee82e1633b363
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-10 17:09:51 +0100

    modalcreateroom

---
+++ src/components/Modal.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import styles from '../styles/components/Modal.module.css'
+import { motion } from "framer-motion"
+
+export default function Modal(props) {
+    const { closeModal, body, foot, head, showModalOpacity } = props
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyDown);
+    }, [])
+
+    function handleKeyDown(event) {
+        if (event.key === 'Escape')
+            closeModal()
+    }
+
+    return (
+        <div>
+            <motion.div
+                className={styles.container}
+                initial={{ opacity: 0 }}
+                animate={showModalOpacity ? { opacity: 1 } : { opacity: 0 }}
+                transition={{ duration: showModalOpacity ? 0.5 : 0.3, easings: ["easeInOut"] }}
+            >
+                <div className={styles.background} onClick={closeModal}>
+                </div>
+                <motion.div
+                    className={styles.modal}
+                    initial={{ scale: 0.7 }}
+                    animate={showModalOpacity ? { scale: 1 } : { scale: 0.7 }}
+                    transition={{ duration: showModalOpacity ? 0.5 : 0.3, easings: ["easeInOut"] }}
+                >
+                    <div className={styles.content}>
+                        <div className={styles.head}>{head}</div>
+                        <div className={styles.body}>{body}</div>
+                        <div className={styles.foot}>{foot}</div>
+                    </div>
+                </motion.div>
+            </motion.div>
+        </div>
+    )
+}
--- src/pages/lobby.js
+++ src/pages/lobby.js
@@ -1,11 +1,22 @@
 import Router from "next/router";
 import { useState } from "react";
+import styles from '../styles/lobby.module.css'
+import Modal from "@/components/Modal";
+import Select from "react-select";
 
 let socket;
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 
 export default function Lobby() {
     const [code, setCode] = useState('')
     const [requestState, setRequestState] = useState('denied')
+    const [showModal, setShowModal] = useState(false)
+    const [showModalOpacity, setShowModalOpacity] = useState(false)
 
     function handleCodeChange(event) {
         setCode(event.target.value)
@@ -20,7 +31,7 @@ export default function Lobby() {
             .then(response => response.json())
             .then(response => {
                 console.log(response)
-                if(response.room){
+                if (response.room) {
                     Router.push(`/room?code=${code}`)
                 }
                 else {
@@ -32,12 +43,53 @@ export default function Lobby() {
         setCode('')
     }
 
+    function createNewRoom() {
+
+    }
+
+    function openModal() {
+        setShowModalOpacity(true)
+        setShowModal(true)
+    }
+
+    function closeModal() {
+        setShowModalOpacity(false)
+        setTimeout(() => setShowModal(false), 300)
+    }
+
     return (
         <div>
             <main>
+                {showModal && <Modal closeModal={closeModal} showModalOpacity={showModalOpacity}
+                    head={
+                        <div>
+                            <h2>Criar Game</h2>
+                        </div>
+                    }
+                    body={
+                        <div>
+                            <label>Nome: </label>
+                            <input />
+                            <label>Private: </label>
+                            <input type="checkbox" />
+                            <Select
+                                placeholder='Quiz'
+                                onChange={() => console.log('')}
+                                options={options}
+                            />
+                        </div>
+                    }
+                    foot={
+                        <div>
+                            <button onClick={closeModal}>Cancelar</button>
+                            <button onClick={createNewRoom}>Criar</button>
+                        </div>
+                    }
+                />}
                 <input onChange={handleCodeChange} value={code} />
                 <button onClick={handleSubmitCode}>Entrar</button>
-                {requestState === 'fail' && <p>Essa sala não existe</p>}
+                <button onClick={openModal}>Criar Game</button>
+                {requestState === 'fail' && <p>Esta sala não existe</p>}
             </main>
         </div>
     );
