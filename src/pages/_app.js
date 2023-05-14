e02b77d802f603e3c5da7419b85cc8d58d4d318a
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-14 22:11:56 +0100

    nomesFemininos

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { SessionProvider } from 'next-auth/react'
 import DataHandler from '@/components/DataHandler'
 import { ToastContainer, Flip } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head'
 
 function MyApp(props) {
   const { Component, pageProps } = props
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import styles from '../styles/index.module.css'
-import Head from 'next/head'
 
 export default function Index(props) {
   const { session } = props
