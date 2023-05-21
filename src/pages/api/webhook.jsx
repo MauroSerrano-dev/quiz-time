68074f5683b9415c4b9e3fdfdd192b12365e7720
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-21 07:05:57 +0100

    webhook

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -8,12 +8,8 @@ export default async function handler(req, res) {
     }
 
     else if (req.method === "POST") {
-        console.log(req.body)
-        console.log(req.body.payment_link)
-        console.log(req.body.data.customer_details)
-        console.log(req.body.data.object.customer_details)
-        const link = req.body.object.payment_link
-        const email = req.body.object.data.customer_details.email
+        const link = req.body.data.object.payment_link
+        const email = req.body.data.object.customer_details.email
         try {
             let plan
             if (link === "plink_1NA3MIHqx2KsFA9zcbgjXuBF")
