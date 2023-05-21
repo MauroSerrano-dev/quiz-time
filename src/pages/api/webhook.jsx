b957b1969985cad0511b4e84b335e3cd7da89523
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-21 07:22:29 +0100

    webhook

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -10,18 +10,13 @@ export default async function handler(req, res) {
     else if (req.method === "POST") {
         const link = req.body.data.object.payment_link
         const email = req.body.data.object.customer_details.email
-        try {
-            let plan
-            if (link === "plink_1NA3MIHqx2KsFA9zcbgjXuBF")
-                plan = 'Premium'
-            if (link === "plink_1NA4OTHqx2KsFA9zesGMYepK")
-                plan = 'Silver'
-            if (plan)
-                updateUserPlan(email, plan)
-        }
-        catch (error) {
-            console.error(`Error: ${error}`);
-        }
+        let plan
+        if (link === "plink_1NA3MIHqx2KsFA9zcbgjXuBF")
+            plan = 'Premium'
+        if (link === "plink_1NA4OTHqx2KsFA9zesGMYepK")
+            plan = 'Silver'
+        if (plan)
+            updateUserPlan(email, plan)
         res.status(201).json({ message: `Plano do User ${email} Atualizado para ${plan} com Sucesso!` })
     }
 
