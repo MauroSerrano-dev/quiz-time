bf0ed32b9cf9cc12deffc07cc0100ff4c23d14af
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-21 23:51:01 +0100

    webhook

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -6,6 +6,6 @@ export default async function handler(req, res) {
         const email = req.body.data.object.customer_details.email
         const plan = req.body.data.object.metadata.plan
             updateUserPlan(email, plan)
-        res.status(201).json({ message: req.body })
+        res.status(201).json({ message: `Plano do User ${email} Atualizado para ${plan} com Sucesso!` })
     }
 }
