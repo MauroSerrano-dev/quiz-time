65e9c7b040ba7d6bcb3b1a726ca7a9d734c1b888
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-23 21:51:25 +0100

    inicio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,10 +11,20 @@ export default function Index(props) {
       <main>
         <section className={styles.demo}>
           <div className={styles.demoTitle}>
-            <h1>Exemplo de title</h1>
-            <p>Exemplo de paragrafo</p>
+            <h1>Crowd de palestra com um quiz mudando randomicamente o design e perguntas e respostas</h1>
+            <p>Transforme suas apresentações mais interativas e divertidas, seja lembrado, seja marcante</p>
           </div>
           <img src='/pc-cel.png' className={styles.pcImg} />
+        </section>
+        <section className={styles.demo}>
+          <div className={styles.demoTitle}>
+            <h1>Recolha os dados do seu publico, os resultados, nomes, emails</h1>
+          </div>
+        </section>
+        <section className={styles.demo}>
+          <div className={styles.demoTitle}>
+            <h1>Capture emails do seu público 24/7</h1>
+          </div>
         </section>
       </main>
     </div>
