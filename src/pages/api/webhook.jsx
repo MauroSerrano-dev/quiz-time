190c84bf24f35790d549b6f81dbdb49df20329e4
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-23 05:16:07 +0100

    stripe

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -3,10 +3,10 @@ import { updateUserPlan } from "../../backend-data/users";
 export default async function handler(req, res) {
 
     if (req.method === "POST") {
-        const email = req.body.data.object.customer_details.email
-        const plan = req.body.data.object.metadata.plan
         const type = req.body.data.type
         if (type === 'checkout.session.completed') {
+            const email = req.body.data.object.customer_details.email
+            const plan = req.body.data.object.metadata.plan
             updateUserPlan(email, plan)
             res.status(201).json({ message: `Plano do User ${email} Atualizado para ${plan} com Sucesso!` })
         }
