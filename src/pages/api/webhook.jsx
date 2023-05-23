7a2ba0a696b866b2a0bcd4bd0702e94b78e410a1
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-23 05:19:52 +0100

    stripe

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -3,7 +3,7 @@ import { updateUserPlan } from "../../backend-data/users";
 export default async function handler(req, res) {
 
     if (req.method === "POST") {
-        const type = req.body.data.type
+        const type = req.body.type
         if (type === 'checkout.session.completed') {
             const email = req.body.data.object.customer_details.email
             const plan = req.body.data.object.metadata.plan
