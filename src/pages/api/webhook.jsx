0bb1d6d344a4f97d64ee1ed2b32ac4f830b51da4
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-23 05:16:40 +0100

    stripe

--- src/pages/api/webhook.jsx
+++ src/pages/api/webhook.jsx
@@ -11,12 +11,12 @@ export default async function handler(req, res) {
             res.status(201).json({ message: `Plano do User ${email} Atualizado para ${plan} com Sucesso!` })
         }
         else if (type === 'subscription_schedule.canceled')
-            res.status(201).json({ message: `Plano do User ${email} subscription_schedule.canceled!` })
+            res.status(201).json({ message: 'subscription_schedule.canceled!' })
         else if (type === 'customer.subscription.updated')
-            res.status(201).json({ message: `Plano do User ${email} customer.subscription.updated!` })
+            res.status(201).json({ message: 'customer.subscription.updated!' })
         else if (type === 'customer.subscription.deleted')
-            res.status(201).json({ message: `Plano do User ${email} customer.subscription.deleted!` })
+            res.status(201).json({ message: 'customer.subscription.deleted!' })
         else if (type === 'customer.subscription.created')
-            res.status(201).json({ message: `Plano do User ${email} customer.subscription.created!` })
+            res.status(201).json({ message: 'customer.subscription.created!' })
     }
 }
