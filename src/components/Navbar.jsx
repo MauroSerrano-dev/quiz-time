cefc06bf616b1580ced82de1fe91db793d0c7037
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-24 02:22:00 +0100

    mainIndex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,47 +19,45 @@ export default function Navbar(props) {
     const [isOpen, toggleOpen] = useCycle(false, true);
 
     return (
-        <div>
-            <div className={styles.container}>
-                <div className={styles.leftSide}>
-                    {/* <MenuToggle toggle={() => toggleOpen()} /> */}
-                    <Link legacyBehavior href={'/'}>
-                        <a>
-                            <img className={styles.logo} src='/quiz-time-logo.png' />
-                        </a>
-                    </Link>
-                </div>
-                <div className={styles.middle}>
-                    <div className={`${navActive ? styles.active : ''} ${styles.nav_menu_list}`}>
-                        {MENU_LIST.map((option, i) =>
-                            <div onClick={() => { setNavActive(false) }}
-                                key={option.name}>
-                                <Link legacyBehavior href={option.href}>
-                                    <a>
-                                        <h1 className={`${styles.navOption} ${option.href === pathname ? styles.currentPage : ''}`}>{option.name}</h1>
-                                    </a>
-                                </Link>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className={styles.rightSide}>
-                    {session &&
-                        <div className={styles.rightSideLogin}>
-                            <h2>{session.user.plan.name}</h2>
-                            <AvatarMenu session={session} signOut={signOut} />
+        <div className={styles.container}>
+            <div className={styles.leftSide}>
+                {/* <MenuToggle toggle={() => toggleOpen()} /> */}
+                <Link legacyBehavior href={'/'}>
+                    <a>
+                        <img className={styles.logo} src='/quiz-time-logo.png' />
+                    </a>
+                </Link>
+            </div>
+            <div className={styles.middle}>
+                <div className={`${navActive ? styles.active : ''} ${styles.nav_menu_list}`}>
+                    {MENU_LIST.map((option, i) =>
+                        <div onClick={() => { setNavActive(false) }}
+                            key={option.name}>
+                            <Link legacyBehavior href={option.href}>
+                                <a>
+                                    <h1 className={`${styles.navOption} ${option.href === pathname ? styles.currentPage : ''}`}>{option.name}</h1>
+                                </a>
+                            </Link>
                         </div>
-                    }
-                    {!session &&
-                        <div>
-                            <Button variant="outlined" className={styles.loginButton} onClick={() => signIn()}>Sign in</Button>
-                        </div>
-                    }
-                    <div onClick={() => setNavActive(!navActive)} className={styles.nav_menu_bar}>
-                        <div className={`${navActive ? styles.active2 : styles.disactive} ${styles.navButton}`}></div>
-                        <div className={`${navActive ? styles.active : styles.disactive2} ${styles.navButton}`}></div>
-                        <div className={`${navActive ? styles.active2 : styles.disactive3} ${styles.navButton}`}></div>
+                    )}
+                </div>
+            </div>
+            <div className={styles.rightSide}>
+                {session &&
+                    <div className={styles.rightSideLogin}>
+                        <h2>{session.user.plan.name}</h2>
+                        <AvatarMenu session={session} signOut={signOut} />
                     </div>
+                }
+                {!session &&
+                    <div>
+                        <Button variant="outlined" className={styles.loginButton} onClick={() => signIn()}>Sign in</Button>
+                    </div>
+                }
+                <div onClick={() => setNavActive(!navActive)} className={styles.nav_menu_bar}>
+                    <div className={`${navActive ? styles.active2 : styles.disactive} ${styles.navButton}`}></div>
+                    <div className={`${navActive ? styles.active : styles.disactive2} ${styles.navButton}`}></div>
+                    <div className={`${navActive ? styles.active2 : styles.disactive3} ${styles.navButton}`}></div>
                 </div>
             </div>
         </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/index.module.css'
+import { motion } from "framer-motion"
 
 export default function Index(props) {
   const { session } = props
@@ -8,31 +9,36 @@ export default function Index(props) {
     <div>
       <Head>
       </Head>
-      <main>
-        <section className={`${styles.demo} ${styles.one}`}>
+      <motion.div
+        className={styles.sectionsContainer}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5, easings: ["easeInOut"] }}
+      >
+        <section className={`${styles.section} ${styles.one}`}>
           <div className={styles.crowdContainer}>
             <img src='/crowd.jpg' className={styles.crowdImg} />
             <img src='/tela.jpg' className={styles.telaImg} />
           </div>
         </section>
-        <section className={styles.demo}>
+        <section className={styles.section}>
           <div className={styles.demoTitle}>
             <h1>Crowd de palestra com um quiz mudando randomicamente o design e perguntas e respostas</h1>
             <p>Transforme suas apresentações mais interativas e divertidas, seja lembrado, seja marcante</p>
           </div>
           <img src='/pc-cel.png' className={styles.pcCelImg} />
         </section>
-        <section className={styles.demo}>
+        <section className={styles.section}>
           <div className={styles.demoTitle}>
             <h1>Recolha os dados do seu publico, os resultados, nomes, emails</h1>
           </div>
         </section>
-        <section className={styles.demo}>
+        <section className={styles.section}>
           <div className={styles.demoTitle}>
             <h1>Capture emails do seu público 24/7</h1>
           </div>
         </section>
-      </main>
-    </div>
+      </motion.div>
+    </div >
   );
 }
