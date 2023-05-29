633d105f0eb7590e26a269b152fc80abeeecd42b
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-29 15:45:43 +0100

    muitaCoisa

--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -31,7 +31,7 @@ export default function PlayersList(props) {
             </div>
             <div id={styles.playersContainer}>
                 <div id={styles.players}>
-                    {players.concat(players).concat(players).concat(players).map((player, i) =>
+                    {players.map((player, i) =>
                         <PlayerCard key={`Player: ${i}`} player={player} progress={(player.answers.length / totalQuestions) * 100} ></PlayerCard>
                     )}
                 </div>
