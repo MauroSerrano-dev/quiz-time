a935a2c4f922a9811c27aa5a1977c0379be17a83
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-30 08:52:58 +0100

    teste

--- src/components/DataHandler.jsx
+++ src/components/DataHandler.jsx
@@ -1,7 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 import Navbar from './Navbar'
 import styles from '../styles/components/DataHandler.module.css'
-import { useEffect } from "react"
 import { Button } from '@mui/material';
 
 const FREE_PAGES = [
@@ -12,17 +11,18 @@ const FREE_PAGES = [
 export default function DataHandler(props) {
     const { Component, pageProps } = props
     const { data: session } = useSession()
+    const [pageName, setPageName] = (Component.name)
 
     return (
         <div>
-            {session === null &&
+            {session === null && !FREE_PAGES.includes(pageName) &&
                 <div id={styles.noSessionContainer}>
                     <img id={styles.logo} src='/quiz-time-logo.png' />
                     <Button variant="outlined" onClick={() => signIn()}>Sign in</Button>
                 </div>
             }
             {
-                session &&
+                (session || FREE_PAGES.includes(pageName)) &&
                 <div>
                     <Navbar session={session} signIn={signIn} signOut={signOut} />
                     <div id={styles.componentContainer} >
