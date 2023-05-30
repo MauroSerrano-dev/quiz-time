81dee976bffa32c13ebbb2a74f31e715b5af4ddd
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-30 09:33:56 +0100

    teste

--- src/components/NoSessionPage.jsx
+++ src/components/NoSessionPage.jsx
@@ -2,12 +2,13 @@
 import styles from '../styles/components/NoSessionPage.module.css'
 import { Button } from '@mui/material';
 
-export default function NoSessionPage() {
+export default function NoSessionPage(props) {
+    const { signIn } = props
 
     return (
         <div id={styles.noSessionContainer}>
             <img id={styles.logo} src='/quiz-time-logo.png' />
-            <Button variant="outlined" onClick={() => signIn()}>Sign in</Button>
+            <Button variant="outlined" onClick={signIn}>Sign in</Button>
         </div>
     )
 }
