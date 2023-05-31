bac49785dab59988ff55c5652de4f93805e0242c
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-05-31 09:02:27 +0100

    tv

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,19 +23,25 @@ export default function Index(props) {
             </div>
           </div>
           <div className={styles.section}>
-            <div id={styles.demoTitle}>
+            <div id={styles.halfSection}>
               <h1>Crowd de palestra com um quiz mudando randomicamente o design e perguntas e respostas</h1>
               <p>Transforme suas apresentações mais interativas e divertidas, seja lembrado, seja marcante</p>
             </div>
             <img src='/pc-cel.png' id={styles.pcCelImg} />
           </div>
           <div className={styles.section}>
-            <div id={styles.demoTitle}>
+            <div id={styles.halfSection}>
+              <div id={styles.tvContainer}>
+                <img src='/crowd.jpg' id={styles.insideTvImg} />
+                <img src='/tv-mobile.png' id={styles.tvImg} />
+              </div>
+            </div>
+            <div id={styles.halfSection}>
               <h1>Recolha os dados do seu publico, os resultados, nomes, emails</h1>
             </div>
           </div>
           <div className={styles.section}>
-            <div id={styles.demoTitle}>
+            <div id={styles.halfSection}>
               <h1>Capture emails do seu público 24/7</h1>
             </div>
           </div>
