a37caf44cf49bd496ebb7455e737b341519de4f4
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-06-29 19:03:12 +0100

    commentsDelete

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -63,8 +63,6 @@ function MyApp(props) {
         <Script src="https://js.stripe.com/v3/" async></Script>
       </Head>
       <ThemeProvider theme={mainTheme}>
-        {/* <div id='blob'></div>
-        <div id='blur'></div> */}
         <SessionProvider>
           <DataHandler pageProps={pageProps} Component={Component} />
           <ToastContainer newestOnTop transition={Flip} style={{ color: 'white' }} className="foo" />
