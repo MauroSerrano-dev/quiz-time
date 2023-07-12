5de06e2e6333f9ec96b9a4c8962ea04fc22043e8
Mauro Serrano <mauro.serrano@hotmail.com.br>
2023-07-12 02:35:53 +0100

    vulnerabilitiesFix

--- src/pages/api/rooms.js
+++ src/pages/api/rooms.js
@@ -1,4 +1,4 @@
-import { getRoom, updateRoom, addRoom } from "../../backend-data/rooms";
+import { getRoom, addRoom } from "../../backend-data/rooms";
 
 export default async function handler(req, res) {
 
@@ -13,14 +13,6 @@ export default async function handler(req, res) {
         res.status(201).json({
             message: 'Room Criada com Sucesso!',
             _id: id,
-        })
-    }
-
-    else if (req.method === "PATCH") {
-        const { room } = req.body
-        updateRoom(room)
-        res.status(200).json({
-            message: "Room Atualizada com Sucesso!"
         })
     }
 }
